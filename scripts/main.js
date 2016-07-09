284791639e2ea744af4db515556248fff7e3d41e
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-07-09 16:09:38 -0700

    added animate feature to move both player images

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,51 @@ $(document).on("ready", function(){
 
 
 
-$(document).keyup(function(e) { //This button
-    if(e.which == 48) {
-        $('.one').show();
-      console.log('test123123123')
+  $(document).keyup(function move(e) { //This button
+    if(e.which == 57) {
+        $( ".hassle" ).animate({"left": "+=50",
 
+        });
+      console.log('Working here')
     }
-});
+  });
+
+
+  $(document).keyup(function move(e) { //This button
+    if(e.which == 49) {
+        $( ".hassle1" ).animate({"left": "+=50",
+
+        });
+      console.log('Working here')
+    }
+  });
+
+  });
+
+  https://api.jquery.com/clearQueue/
+  http://api.jquery.com/width/
+  https://api.jquery.com/jquery.when/
+  http://api.jquery.com/offset/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 // //================================================================================================================================================
+
+
+// Problem : Current setup consists of accessability to only one image class.
+
+//Solution: move the image instead of hiding and showing
