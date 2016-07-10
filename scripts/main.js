d07398f5048cdf58e2308b31990dc7e53f6a1735
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-07-10 11:57:04 -0700

    add function that recognizes winner

--- scripts/main.js
+++ scripts/main.js
@@ -5,28 +5,46 @@ $(document).on("ready", function() {
     if (e.which == 49) {
       $(".hassle").animate({
         "left": "+=50",
-
-        // $( ".hassle:last" ).text( hassle.left );
       });
+      if ($('.buchannon').width() <= $('.hassle').offset().left){
+        // buchannonWin();             //This function will trigger the event of Mitch winning
+
+        alert('Mitch Wins!');          //
+      }
     }
   });
 
-  $(document).keyup(function move(e) { //Knight Rider
-    if (e.which == 57) {
+
+  $(document).keyup(function move(e) { //Michael Knight
+    if (e.which == 48) {
       $(".hassle1").animate({
-        "left": "+=50",
-
-        // if ("left" = 600px) {
-        //   console.log('Working')
-        // }
+        "left": "+=60",
       });
-
-
+      if ($('.knight').width() <= $('.hassle1').offset().left){
+        // knightWin();                //This function will trigger the event of Michael winning
+                                       //think about adding to scoreboard and revealing a hidden window and also music
+                                       //TODO: make boilerplate function that both- What features do I want to add for both?
+        alert('Michael Wins!!');
+      }
     }
   });
-  // var h = $('.hassle')
-  // var hassle = h.position()
+
+
+
+
 });
+
+//think about adding to scoreboard and revealing a hidden window and also music
+//TODO: make boilerplate function that both use- What features do I want to add for both?
+//TODO: Win counter function for both? Might have to be different \.
+
+
+
+
+// Problem : Current setup consists of accessability to only one image class.
+
+//Solution: move the image instead of hiding and showing
+
 
 
 
@@ -45,8 +63,3 @@ $(document).on("ready", function() {
 
 
 // //================================================================================================================================================
-
-
-// Problem : Current setup consists of accessability to only one image class.
-
-//Solution: move the image instead of hiding and showing
