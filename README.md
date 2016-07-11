ae300f19875a28b6ad016719124fa10114321fcd
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-07-11 08:10:26 -0700

    add layout for scoreboard, add minor styling, change readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 README.md
 
-HassleOff is a game that pits two of David Hasslehoff's greates characters against each other in a deadly race.
+HassleOff is a game that pits two of David Hasslehoff's greatest characters against each other in a deadly race.
+
+The controls for HassleOff are binary! Tap "1" and "0" to move player one and two respectively.
+
+The champion will be notified by the sounding of their theme song. 
--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,38 @@
 
 $(document).on("ready", function() {
-
   $(document).keyup(function move(e) { //Mitch Buchannon
     if (e.which == 49) {
-      $(".hassle").css("left",("+=50"));
-
-
-
+      $(".hassle").css("left",("+=40"));
       if ($track1.width() <= $player1.offset().left + $player1.outerWidth()){
         fullStop();
+        hassle.wins += 1;
+        console.log(hassle.wins);
         $('#buzzer1').get(0).play();
       }
     }
-  });
 
+  });
   $(document).keyup(function move(e) { //Michael Knight
     if (e.which == 48) {
-      $(".hassle1").css("left",("+=50"));
+      $(".hassle1").css("left",("+=55"));
       if ($track2.width() <= $player2.offset().left + $player2.outerWidth()){
         fullStop();
+        hassle1.wins +=1;
+        // $('.p1').append("<p>" + hassle1.wins + "</p>" ); Scoreboard logic, refactor later
         $('#buzzer').get(0).play();
       }
     }
   });
 
-
+  var hassle = new Count("+=1");
+  var hassle1 = new Count("+=1");
 
   var $track1 = $('.buchannon'); // Player 1 race track div
   var $track2 = $('.knight'); // player 2 race track div
   var $player1 = $('.hassle'); // Player 1 image
   var $player2 = $('.hassle1'); // Player 2 image
 
-  // function knightWin(){
-  //   fullStop();
 
-  // }
-  //
-  // function bayWin(){
-  //   fullStop();
-  // }
 
   function fullStop(){
     $player1.clearQueue();       //This function clears any built up keystrokes
@@ -48,12 +42,24 @@ $(document).on("ready", function() {
     $(document).off('keyup');
   }
 
+function Count(counter){
+  this.wins = 0;
+
+}
+
 });
 
 //TODO: buchannonWin() and knightWin() both should reveal hidden window, music
 //TODO: make boilerplate function that both use- What features do I want to add for both?
 //TODO: Win counter function for both? Might have to be different
-//TODO: Address bug that causes players to extend beyond div container
+//TODO: Finish Scoreboard logic
+//TODO: fix parallax bug 
+
+
+
+
+
+
 
 
 
